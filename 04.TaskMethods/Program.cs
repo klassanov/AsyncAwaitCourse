42871eaa9569c2b127cb21a1cf562eafd4bc0867
Alexander Klassanov <klassanov@gmail.com>
2022-01-04 10:03:06 +0200

    More task examples

--- 04.TaskMethods/Program.cs
+++ 04.TaskMethods/Program.cs
@@ -9,7 +9,10 @@ namespace _04.TaskMethods
         {
             //WaitForTask();
             //TaskContinuation();
-            TaskExceptionsAndStatus();
+            //TaskExceptionsAndStatus();
+            //MultipleTasksAtTheSameTime();
+            //AtLeastOneTaskToFinish();
+            CompletedTaskAndFromResult();
         }
 
         static void WaitForTask()
@@ -72,6 +75,104 @@ namespace _04.TaskMethods
                 });
 
             task.Wait();
+        }
+
+        static void MultipleTasksAtTheSameTime()
+        {
+            var firstTask = Task.Run(() => Task.Delay(3000).Wait())
+                            .ContinueWith(_ => Console.WriteLine("First"));
+
+            var secondTask = Task.Run(() => Task.Delay(1000).Wait())
+                            .ContinueWith(_ => Console.WriteLine("Second"));
+
+            var thirdTask = Task.Run(() => Task.Delay(2000).Wait())
+                            .ContinueWith(_ => Console.WriteLine("Third"));
+
+            //This also blocks and waits until all of them are executed, but they are executed at the same time, 
+            //so the time we wait is equal to the execution time of the longest task
+            Task.WaitAll(firstTask, secondTask, thirdTask);
+
+        }
+
+        static void AtLeastOneTaskToFinish()
+        {
+            //Operation timeout implementation: the one that finishes first
+
+            //Note how even though the console output might be broken, the input and output are not confused
+            Console.WriteLine("You have 10 seconds to solve 10*10");
+
+
+            var inputTask = Task.Run(() =>
+            {
+                while (true)
+                {
+                    var answer = Console.ReadLine();
+                    if (answer.Equals("100"))
+                    {
+                        Console.WriteLine("Correct!");
+                        break;
+                    }
+                    Console.WriteLine("Wrong answer");
+                }
+            });
+
+            var timerTask = Task.Run(() =>
+            {
+                for (int i = 10; i > 0; i--)
+                {
+                    Console.WriteLine(i);
+
+                    Task.Delay(1000).Wait();
+                }
+
+                Console.WriteLine("Timeout");
+            });
+
+            //Operation Timer implementation
+            Task.WaitAny(timerTask, inputTask);
+        }
+
+        static void CompletedTaskAndFromResult()
+        {
+            //Example of CompletedTask, FromResult, FromException => useful when we have the return result ready and
+            //there is no sense in launching a new complex long-running task
+            Console.WriteLine("Please enter your input");
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                if (input == "end")
+                {
+                    break;
+                }
+
+                //New switch syntax
+                var task = input switch
+                {
+                    "delay" => Task
+                            .Delay(3000)
+                            .ContinueWith(_ => Console.WriteLine("Delayed")),
+
+                    "print" => Task
+                            .Run(() => Console.WriteLine("Printed")),
+
+                    "throw" => Task
+                            .FromException(new InvalidOperationException("Some error on invalid operation"))
+                            .ContinueWith(prev => Console.WriteLine(prev.Exception.Message)),
+
+                    "76" => Task
+                            .FromResult(76)
+                            .ContinueWith(prev => Console.WriteLine(prev.Result)),
+
+                    //default case
+                    _ => Task
+                         .CompletedTask
+                         .ContinueWith(_ => Console.WriteLine("Invalid input"))
+                };
+
+                task.Wait();
+            }
         }
     }
 }
