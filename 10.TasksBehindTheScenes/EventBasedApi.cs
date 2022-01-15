db585a985076636893536709e3db2de91c6b16fe
Alexander Klassanov <klassanov@gmail.com>
2022-01-15 22:11:08 +0200

    What happens behind the scenes of a task

---
+++ 10.TasksBehindTheScenes/EventBasedApi.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _10.TasksBehindTheScenes
+{
+    class EventBasedApi
+    {
+        //Defines an event
+        public event Action<string> OnDone;
+
+        public void Work()
+        {
+            Console.WriteLine("Working...");
+
+            var end = DateTime.Now.AddSeconds(5);
+
+            while (true)
+            {
+                if (DateTime.Now > end)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("Firing OnDone");
+
+
+            //Raises the event
+            this.OnDone("Data");
+        }
+    }
+}
---
+++ 10.TasksBehindTheScenes/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _10.TasksBehindTheScenes
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //var result = await RunAsync(() => "RunAsync task");
+            //Console.WriteLine(result);
+
+            //await DelayAsync(2000, () => Console.WriteLine("Run delayed task"));
+
+
+            //How to wrap in async/await event based apis
+
+            //Classical usage
+            //EventBasedApiClassical();
+
+            //Async await wrapper usage
+            var result = await EventBasedApiWrapperAsync();
+            Console.WriteLine(result);
+
+        }
+
+        static void EventBasedApiClassical()
+        {
+            EventBasedApi api = new EventBasedApi();
+            api.OnDone += args =>
+            {
+                Console.WriteLine(args);
+            };
+            api.Work();
+        }
+
+
+        static Task<string> EventBasedApiWrapperAsync()
+        {
+            var tcs = new TaskCompletionSource<string>();
+            var obj = new EventBasedApi();
+            obj.OnDone += args =>
+            {
+                //SetResult or SetException completes the task
+                //This will notify the caller of the EventBasedApiWrapper that the task just completed.
+                tcs.SetResult(args);
+            };
+
+            //You have the full control
+            try
+            {
+                obj.Work();
+            }
+            catch (Exception ex)
+            {
+                tcs.SetException(ex);
+                throw;
+            }
+
+            return tcs.Task;
+        }
+
+
+        public static Task<T> RunAsync<T>(Func<T> function)
+        {
+            if (function == null)
+            {
+                throw new InvalidOperationException("function is null");
+            }
+
+            var completionSource = new TaskCompletionSource<T>();
+
+            ThreadPool.QueueUserWorkItem(_ =>
+            {
+                try
+                {
+                    T result = function();
+                    completionSource.SetResult(result);
+                }
+
+                catch (Exception ex)
+                {
+                    completionSource.SetException(ex);
+                }
+            });
+
+            return completionSource.Task;
+        }
+
+        static Task DelayAsync(int milliseconds, Action action)
+        {
+            if (action == null)
+            {
+                throw new InvalidOperationException("action is null");
+            }
+
+            var tcs = new TaskCompletionSource<object>();
+
+            var timer = new Timer(
+                _ => tcs.SetResult(null),
+                null,
+                milliseconds,
+                Timeout.Infinite);
+
+            return tcs.Task.ContinueWith(_ =>
+            {
+                timer.Dispose();
+                action();
+            });
+        }
+    }
+}
