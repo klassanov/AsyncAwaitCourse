70a20e65756335c8321571fd7da2e21d569695ef
Alexander Klassanov <klassanov@gmail.com>
2022-01-15 22:37:50 +0200

    CustomAwater

---
+++ CustomAwait/Awaiters.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace CustomAwait
+{
+    public static class Awaiters
+    {
+        //Everything that has a method named GetAwaiter can be awaited.
+        //So, we define these methods and then we await
+        public static TaskAwaiter<string> GetAwaiter(this Uri uri)
+        {
+            return new HttpClient().GetStringAsync(uri).GetAwaiter();
+        }
+
+        //Cool syntax
+        public static TaskAwaiter GetAwaiter(this TimeSpan timeSpan)
+            => Task.Delay(timeSpan).GetAwaiter();
+
+
+        public static TaskAwaiter GetAwaiter(this IEnumerable<Task> tasks)
+            => Task.WhenAll(tasks).GetAwaiter();
+
+    }
+}
---
+++ CustomAwait/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CustomAwait
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var content = await new Uri("http://abv.bg");
+            Console.WriteLine(content);
+
+
+            await TimeSpan.FromMilliseconds(1000);
+
+
+            await new List<Task>()
+            {
+                Task.Delay(1000),
+                Task.Delay(2000)
+            };
+        }
+    }
+}
